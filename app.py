96829d019681f28653a732c0ff5b66d17c313d9a
Juan <jg.zuritam@uea.edu.ec>
2026-02-14 23:40:07 -0500

    Fix puerto para Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
@@ -10,4 +11,5 @@ def libro(LOQUEAGUASELLEVO):
     return f"Libro: {LOQUEAGUASELLEVO} – Consulta cualquie libro en la  BIBLIOTECA UEA."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
